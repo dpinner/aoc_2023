cf44cb71a0711174c90ecd0c384426f3f89def7a
dpinner <66742635+dpinner@users.noreply.github.com>
2023-12-08 21:35:11 -0800

    day 9

---
+++ day09/day09.py
@@ -0,0 +1,30 @@
+import sys
+
+def extrapolate(series, prefix):
+    stack = [series if not prefix else list(reversed(series))]
+    if all((v == 0 for v in stack[-1])):
+        return 0
+    while any((v != 0 for v in stack[-1])):
+        stack.append([b-a for a,b in zip(stack[-1],stack[-1][1:])])
+    delta = stack.pop()[-1]
+    while len(stack) > 1:
+        stack[-1].append(stack[-1][-1] + delta)
+        delta = stack.pop()[-1]
+    return stack[-1][-1] + delta
+
+
+def sum_extrapolate(filename: str, prefix: bool) -> int:
+    tot = 0
+    with open(filename, "r") as f:
+        for line in f:
+            l = line.rstrip()
+            if not l:
+                continue
+            tot += extrapolate([int(v) for v in l.split()], prefix)
+        return tot
+
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    print(sum_extrapolate(filename, False))
+    print(sum_extrapolate(filename, True))
